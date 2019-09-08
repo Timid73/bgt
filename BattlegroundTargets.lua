65eed881a24404096a4949fb6418debc9dad681d
Timid <timid73@bk.ru>
2019-09-09 02:06:39 +0400

    add check spec

--- BattlegroundTargets.lua
+++ BattlegroundTargets.lua
@@ -111,6 +111,10 @@
 BattlegroundTargets_Options = {};
 local BattlegroundTargets = CreateFrame("Frame");
 
+local dd = "DD";
+local healer = "Healer";
+local tank = "Tank";
+
 local L = BattlegroundTargets_Localization;
 local BGN = BattlegroundTargets_BGNames;
 local FLG = BattlegroundTargets_Flag;
@@ -121,8 +125,115 @@ local TEMPLATE = {};
 local OPT = {};
 
 local AddonIcon = "Interface\\AddOns\\BattlegroundTargets\\BattlegroundTargets-texture-button";
-local specBuffs = BattlegroundTargets_specBuffs;
-local specSpells = BattlegroundTargets_specSpells;
+
+local bgSpecs = {}
+local specBuffs = {
+    -- WARRIOR
+    [GetSpellInfo(56638)] = dd, -- Taste for Blood
+    [GetSpellInfo(64976)] = dd, -- Juggernaut
+    [GetSpellInfo(29801)] = dd, -- Rampage
+    [GetSpellInfo(50227)] = tank, -- Sword and Board
+    -- PALADIN
+    [GetSpellInfo(20375)] = dd, -- If you are using Seal of Command, I hate you so much
+    [GetSpellInfo(31836)] = healer, -- Light's Grace
+    -- ROGUE
+    [GetSpellInfo(36554)] = dd, -- Shadowstep
+    [GetSpellInfo(31223)] = dd, -- Master of Subtlety
+    -- PRIEST
+    [GetSpellInfo(47788)] = healer, -- Guardian Spirit
+    [GetSpellInfo(52800)] = healer, -- Borrowed Time
+    [GetSpellInfo(15473)] = dd, -- Shadowform
+    [GetSpellInfo(15286)] = dd, -- Vampiric Embrace
+    -- DEATHKNIGHT
+    [GetSpellInfo(49222)] = dd, -- Bone Shield
+    [GetSpellInfo(49016)] = dd, -- Hysteria
+    [GetSpellInfo(53138)] = dd, -- Abomination's Might
+    [GetSpellInfo(55610)] = dd, -- Imp. Icy Talons
+    -- MAGE
+    [GetSpellInfo(43039)] = dd, -- Ice Barrier
+    [GetSpellInfo(11129)] = dd, -- Combustion
+    [GetSpellInfo(31583)] = dd, -- Arcane Empowerment
+    -- WARLOCK
+    [GetSpellInfo(30302)] = dd, -- Nether Protection
+    -- SHAMAN
+    [GetSpellInfo(57663)] = dd, -- Totem of Wrath
+    [GetSpellInfo(49284)] = healer, -- Earth Shield
+    [GetSpellInfo(51470)] = dd, -- Elemental Oath
+    [GetSpellInfo(30809)] = dd, -- Unleashed Rage
+    -- HUNTER
+    [GetSpellInfo(20895)] = dd, -- Spirit Bond
+    [GetSpellInfo(19506)] = dd, -- Trueshot Aura
+    -- DRUID
+    [GetSpellInfo(24932)] = dd, -- Leader of the Pack
+    [GetSpellInfo(34123)] = healer, -- Tree of Life
+    [GetSpellInfo(24907)] = dd, -- Moonkin Aura
+    [GetSpellInfo(53251)] = healer, -- Wild Growth
+}
+local specSpells = {
+    -- WARRIOR
+    [GetSpellInfo(47486)] = dd, -- Mortal Strike
+    [GetSpellInfo(46924)] = dd, -- Bladestorm
+    [GetSpellInfo(23881)] = dd, -- Bloodthirst
+    [GetSpellInfo(12809)] = tank, -- Concussion Blow
+    [GetSpellInfo(47498)] = tank, -- Devastate
+    -- PALADIN
+    [GetSpellInfo(48827)] = tank, -- Avenger's Shield
+    [GetSpellInfo(48825)] = healer, -- Holy Shock
+    [GetSpellInfo(35395)] = dd, -- Crusader Strike
+    [GetSpellInfo(53385)] = dd, -- Divine Storm
+    [GetSpellInfo(20066)] = dd, -- Repentance
+    -- ROGUE
+    [GetSpellInfo(48666)] = dd, -- Mutilate
+    [GetSpellInfo(51690)] = dd, -- Killing Spree
+    [GetSpellInfo(13877)] = dd, -- Blade Flurry
+    [GetSpellInfo(13750)] = dd, -- Adrenaline Rush
+    [GetSpellInfo(48660)] = dd, -- Hemorrhage
+    -- PRIEST
+    [GetSpellInfo(53007)] = healer, -- Penance
+    [GetSpellInfo(10060)] = healer, -- Power Infusion
+    [GetSpellInfo(33206)] = healer, -- Pain Suppression
+    [GetSpellInfo(34861)] = healer, -- Circle of Healing
+    [GetSpellInfo(15487)] = dd, -- Silence
+    [GetSpellInfo(48160)] = dd, -- Vampiric Touch
+    -- DEATHKNIGHT
+    [GetSpellInfo(55262)] = dd, -- Heart Strike
+    [GetSpellInfo(49203)] = dd, -- Hungering Cold
+    [GetSpellInfo(55268)] = dd, -- Frost Strike
+    [GetSpellInfo(51411)] = dd, -- Howling Blast
+    [GetSpellInfo(55271)] = dd, -- Scourge Strike
+    -- MAGE
+    [GetSpellInfo(44781)] = dd, -- Arcane Barrage
+    [GetSpellInfo(55360)] = dd, -- Living Bomb
+    [GetSpellInfo(42950)] = dd, -- Dragon's Breath
+    [GetSpellInfo(42945)] = dd, -- Blast Wave
+    [GetSpellInfo(44572)] = dd, -- Deep Freeze
+    -- WARLOCK
+    [GetSpellInfo(59164)] = dd, -- Haunt
+    [GetSpellInfo(47843)] = dd, -- Unstable Affliction
+    [GetSpellInfo(59672)] = dd, -- Metamorphosis
+    [GetSpellInfo(59172)] = dd, -- Chaos Bolt
+    [GetSpellInfo(47847)] = dd, -- Shadowfury
+    -- SHAMAN
+    [GetSpellInfo(59159)] = dd, -- Thunderstorm
+    [GetSpellInfo(16166)] = dd, -- Elemental Mastery
+    [GetSpellInfo(51533)] = dd, -- Feral Spirit
+    [GetSpellInfo(30823)] = dd, -- Shamanistic Rage
+    [GetSpellInfo(17364)] = dd, -- Stormstrike
+    [GetSpellInfo(61301)] = healer, -- Riptide
+    [GetSpellInfo(51886)] = healer, -- Cleanse Spirit
+    -- HUNTER
+    [GetSpellInfo(19577)] = dd, -- Intimidation
+    [GetSpellInfo(34490)] = dd, -- Silencing Shot
+    [GetSpellInfo(53209)] = dd, -- Chimera Shot
+    [GetSpellInfo(60053)] = dd, -- Explosive Shot
+    [GetSpellInfo(49012)] = dd, -- Wyvern Sting
+    -- DRUID
+    [GetSpellInfo(53201)] = dd, -- Starfall
+    [GetSpellInfo(61384)] = dd, -- Typhoon
+    [GetSpellInfo(48566)] = dd, -- Mangle (Cat)
+    [GetSpellInfo(48564)] = dd, -- Mangle (Bear)
+    [GetSpellInfo(18562)] = healer, -- Swiftmend
+}
 local healers = {};
 
 local _G = _G;
@@ -293,10 +404,6 @@ local classcolors = {};
 for class, color in pairs(RAID_CLASS_COLORS) do
     classcolors[class] = { r = color.r, g = color.g, b = color.b }
 end
-
-local dd = "DD";
-local healer = "Healer";
-local tank = "Tank";
 
 local classes = {
     DEATHKNIGHT = { 0.265625, 0.484375, 0.515625, 0.734375 },
@@ -4887,7 +4994,6 @@ end
 
 function BattlegroundTargets:BattlefieldCheck()
     if (not inWorld) then return; end
-
     local _, instanceType = IsInInstance();
     if instanceType == "pvp" then
         BattlegroundTargets:IsBattleground();
@@ -4900,6 +5006,8 @@ end
 function BattlegroundTargets:IsBattleground()
     inBattleground = true;
     isFlagBG = 0;
+    table_wipe(healers);
+    table_wipe(bgSpecs);
 
     local queueStatus, queueMapName, bgName;
     for i = 1, MAX_BATTLEFIELD_QUEUES do
@@ -5328,7 +5436,7 @@ function BattlegroundTargets:CheckUnitTarget(unitID, unitName)
         enemyName = unitName;
     end
 
-    if (healers[unitID]) then
+    if (healers[enemyID]) then
         enemyName = enemyName .. "- HEALER!!!";
     end
 
@@ -6034,6 +6142,15 @@ local function OnEvent(self, event, ...)
             playerLevel = arg1;
             BattlegroundTargets:CheckPlayerLevel();
         end
+    elseif (event == "UNIT_AURA") then
+        local arg1 = ...;
+        BattlegroundTargets:CheckHealerByAura(arg1)
+    elseif (event == "UNIT_SPELLCAST_START") then
+        local arg1 = ...;
+        BattlegroundTargets:CheckHealerBySpell(arg1)
+    elseif (event == "UNIT_SPELLCAST_SUCCEEDED") then
+        local arg1 = ...;
+        BattlegroundTargets:CheckHealerBySpell(arg1)
     elseif (event == "PLAYER_LOGIN") then
         BattlegroundTargets:CheckFaction();
         BattlegroundTargets:InitOptions();
@@ -6068,18 +6185,28 @@ local function OnEvent(self, event, ...)
     end
 end
 
-function BattlegroundTargets:UNIT_AURA(event, unit)
-    if (ENEMY_Names[unit.name] and not healers[unit]) then
 
+function BattlegroundTargets:CheckHealerBySpell(unitID)
+    local spell, rank, displayName, icon, startTime, endTime, isTradeSkill = UnitCastingInfo(unitID)
+    if (not healers[unitID] and spell) then --ENEMY_Names[unit.name] and
+
+        -- Healer detection
+        BattlegroundTargets:DetectHealer(unitID, specSpells[spell])
+    end
+end
+
+function BattlegroundTargets:CheckHealerByAura(unitID)
+
+    if( not unitID ) then return end
+    if (not healers[unitID]) then
         local index = 1
 
         --Buffs
         while ( true ) do
-            local name, rank, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, unitID = UnitAura(unit, index, "HELPFUL")
+            local name, rank, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable = UnitAura(unitID, index, "HELPFUL")
             if ( not name ) then break end
-
-            -- Spec detection
-            BattlegroundTargets:DetectHealer(unitID, specBuffs[name])
+            -- Healer detection
+            BattlegroundTargets:DetectHealer(unitCaster, specBuffs[name])
 
             index = index + 1
         end
@@ -6087,11 +6214,11 @@ function BattlegroundTargets:UNIT_AURA(event, unit)
 end
 
 function BattlegroundTargets:DetectHealer(unitID, spec)
-    if( not spec ) then return end
-    SELECTED_CHAT_FRAME:AddMessage( "detecting healers" );
+    if( not spec or bgSpecs[unitID]) then return end
+    table.insert(bgSpecs, unitID)
     if (spec == healer) then
+        message(unitID .. " is healer")
         table.insert(healers, unitID)
-        SELECTED_CHAT_FRAME:AddMessage( "healers detected with ID " .. unitID );
     end
 end
 
